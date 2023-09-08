4fc017d1325e826c444898d6695836c8d6b5641d
Srinidhi-codes <srinidhiachar2002@gmail.com>
2023-09-08 21:23:03 +0530

    Version4

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ const getInfo = async (event) => {
     }
     else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=669a5ae1471994313e61a5317bf82518`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=669a5ae1471994313e61a5317bf82518`
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
